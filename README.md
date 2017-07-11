cd2742df424f42308dce78d9fd4d1523d6e7501a
milesaylward <miles.aylward@gmail.com>
2017-07-11 02:32:45 -0400

    app jasmine tests

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-# angular1-quickstart
+# Formstack Sample Application
 
-Kick start your new project.
+Application for converting a float to a decimal.  
+
+
+## Requirements:
+
+* Write and test an Angular 1 directive that:
+  * Receives a model as a float (from 0 - 1)
+  * Displays to the user as a percentage (from 0 - 100)
+  * Would be marked invalid if below 0 or above 100
+
 
 ## Setup
+To clone and edit this repository follow these steps or to test it out a working copy is hosted via github pages  
+[Right Here](https://www.google.com)
 
 1. clone this repository
 2. cd <directory>
@@ -11,3 +22,9 @@ Kick start your new project.
 ## Run
 
 1. npm start
+
+## Tests
+1. cd into the root directory and run npm test
+2. all tests located in the [app.spec.js file](https://github.com/milesaylward/formstack-deploy/blob/master/src/app.spec.js)
+
+### Hope I impressed you Max or whomever is viewing my code from Formstack!
--- src/app.spec.js
+++ src/app.spec.js
@@ -1,3 +1,64 @@
-describe('App Module', function() {
-  
+describe('App Module', () => {
+  var scope,
+    controller;
+    beforeEach(function () {
+        angular.mock.module('app');
+    });
+
+  describe('appController', () => {
+    beforeEach(inject(function ($rootScope, $controller) {
+        scope = $rootScope.$new();
+        controller = $controller('appController', {
+            '$scope': scope
+        });
+      }),
+
+    );
+    /*
+    the following 3 tests ensure that errors are displayed when a user
+    incorrectly submits whether it's a string or out of range.
+    */
+    it('will return an error if a string is passed', () => {
+      scope.submit('hello');
+      expect(scope.error).not.toBe('');
+      });
+    });
+
+    it('will return an error if the number is above 1', () => {
+      scope.submit(2);
+      expect(scope.error).not.toBe('');
+    });
+
+    it('will return an error if the number is below 0', () => {
+      scope.submit(-2);
+      expect(scope.error).not.toBe('');
+    });
+
+    /*
+    when valid input is ensures no error is thrown and the the number
+    is pushed to the percents array
+    */
+
+    it('will not error and will increase percents array length', () => {
+      scope.submit(.54);
+      expect(scope.error).toBe('');
+      expect(scope.percents.length).toBe(1);
+    });
+    /*
+    this will test the random buttons function and because it continues the test
+    above the percents array will have a length of 2
+    */
+    it('will generate a random number and add it to the percents array', () => {
+      scope.random();
+      expect(scope.percents.length).toBe(2);
+    });
+
+    //last test will test clearing the previous 2 additions to the percents array
+    it('will clear the percents array when called', () => {
+      // add item to array just to be sure
+      scope.submit(.54)
+      expect(scope.percents.length).not.toBe(0);
+      scope.clear();
+      expect(scope.percents.length).toBe(0);
+    });
 });
